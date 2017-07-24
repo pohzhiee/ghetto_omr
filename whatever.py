9bcd6e5ca8040f110252137b0923c898a6035478
pohzhiee <poh140996@hotmail.com>
2017-07-24 17:16:43 +0800

    affasd

--- whatever.py
+++ whatever.py
@@ -1,25 +1,14 @@
-import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-import img_func
-import matplotlib
-img = cv2.imread('img_data/oms1.jpg',0)
-edges = cv2.Canny(img,200,255)
-thresh, blurred = img_func.thres1(edges)
-kernel = np.ones((2,2),np.uint8)
-dilated1 = cv2.dilate(thresh,kernel,iterations=3)
-blur_dil1 = cv2.GaussianBlur(dilated1,(5,5),0)
-ero1 = cv2.erode(blur_dil1,kernel,iterations=3)
-dilated2 = cv2.dilate(img,kernel,iterations=1)
-edge2 = cv2.Canny(img,200,255)
-plt.subplot(231),plt.imshow(edges,cmap = 'gray')
-plt.title('edge1 Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(232),plt.imshow(dilated1,cmap = 'gray')
-plt.title('dilate1 Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(233),plt.imshow(blur_dil1,cmap = 'gray')
-plt.title('dilate1 Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(234),plt.imshow(ero1,cmap = 'gray')
-plt.title('dilate2 Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(235),plt.imshow(edge2,cmap = 'gray')
-plt.title('edge2 Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+import cv2
+img = cv2.imread('img_data/oms1.jpg',cv2.IMREAD_GRAYSCALE)
+biblur=cv2.bilateralFilter(img,20,175,175)
+sharp=cv2.addWeighted(img,1.55,biblur,-0.5,0)
+ret1,thresh1 = cv2.threshold(sharp,127,255,cv2.THRESH_OTSU)
+height, width = img.shape[:2]
+im2, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+empt=np.zeros((height,width),dtype=np.uint8)
+cv2.imshow("thresh1",thresh1)
+cv2.imshow("IM2",im2)
+cv2.drawContours(empt, contours, -1, (255,255,255), 3)
+cv2.imshow("CONTOUR",empt)
+cv2.waitKey(0)
