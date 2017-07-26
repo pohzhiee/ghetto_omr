52f732a2dcf3191f800ec9d7aacea2b9b3bdfa4b
edcamcam <edcamcam@hotmail.com>
2017-07-26 20:38:40 +0800

    edcamcam _testfile_imgproc10: Same as 9 Matrix Binarisation and Optimisation [drafted successful]
    Up next:
    Check for bugs and fix them
    Check Spacings and confirm those that are supposed to be added are added
    or else run again
    Fill up those missing ones
    
    Results: Form the optimised grid

--- edcamcam_testfile_temp.py
+++ edcamcam_testfile_temp.py
@@ -6,36 +6,30 @@ import matplotlib.pyplot as plt
 
 #matr_generator
 
-p=1
-q=1
-
-while p<4:
-    while q<4:
-        matr = np.zeros((p, q), dtype=np.uint8)
-        print matr
-        k=0
-        while k < 2**(p+q-1)-1:
-            g=0
-            while g < p*q:
-                i=0
-                j=0
-                while i < p:
-
-                    while j < q:
-                        if matr[i, j] == 1:
-                            matr[i, j] == 0
-                        elif matr[i, j] == 0:
-                            matr[i, j] = 1
-                            print matr
-                            break
-                        j = j + 1
-                    else:
-                        continue
-
+p=2
+q=2
+matr = np.zeros((p, q), dtype=np.uint8)
+print matr
+k=0
+while k < 2**(p+q-1)-1:
+    g=0
+    while g < p*q:
+        i=0
+        j=0
+        while i < p:
+            while j < q:
+                if matr[i, j] == 1:
+                    matr[i, j] == 0
+                elif matr[i, j] == 0:
+                    matr[i, j] = 1
+                    print matr
                     break
-                    i = i + 1
-                g=g+1
-            k=k+1
-        q=q+1
-    p=p+1
+                j = j + 1
+            else:
+                continue
+
+            break
+            i = i + 1
+        g=g+1
+    k=k+1
 
