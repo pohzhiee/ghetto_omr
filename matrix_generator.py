231f6942fa6af26e7a8cea69e881bb94e64676c9
pohzhiee <poh140996@hotmail.com>
2017-07-26 19:34:46 +0800

    completed matrix generator

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,41 +1,13 @@
 import numpy as np
-import cv2
-from edcamcam_testfile_shape import shapedetector
-import matplotlib.pyplot as plt
+import itertools
+p=3
+q=2
+num_bits = p*q
+matr = np.empty((p,q),np.uint8)
+lst = list(itertools.product([0, 1], repeat=num_bits))
+for mat_bin in lst:
+    for i in range(p):
+        matr[i,:]=mat_bin[i*q:(i+1)*q]
+    print matr
 
-
-#matr_generator
-
-p=1
-q=1
-
-while p<4:
-    while q<4:
-        matr = np.zeros((p, q), dtype=np.uint8)
-        print matr
-        k=0
-        while k < 2**(p+q-1)-1:
-            g=0
-            while g < p*q:
-                i=0
-                j=0
-                while i < p:
-
-                    while j < q:
-                        if matr[i, j] == 1:
-                            matr[i, j] == 0
-                        elif matr[i, j] == 0:
-                            matr[i, j] = 1
-                            print matr
-                            break
-                        j = j + 1
-                    else:
-                        continue
-
-                    break
-                    i = i + 1
-                g=g+1
-            k=k+1
-        q=q+1
-    p=p+1
 
