a212f20523a3a7fd10db6f970ce10658bfe95aa6
pohzhiee <poh140996@hotmail.com>
2017-07-26 19:38:00 +0800

    edited variable name

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,13 +1,13 @@
 import numpy as np
 import itertools
 p=3
-q=2
+q=3
 num_bits = p*q
 matr = np.empty((p,q),np.uint8)
 lst = list(itertools.product([0, 1], repeat=num_bits))
-for mat_bin in lst:
+for bin_arr in lst:
     for i in range(p):
-        matr[i,:]=mat_bin[i*q:(i+1)*q]
+        matr[i,:]=bin_arr[i*q:(i+1)*q]
     print matr
 
 
