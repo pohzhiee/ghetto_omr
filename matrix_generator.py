50fe45605fe70632ea82776c3e6f96dad405a0ae
edcamcam <edcamcam@hotmail.com>
2017-07-26 18:32:44 +0800

    edcamcam _testfile_temp: Matrix generation code. Cant work

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,37 +1,41 @@
 import numpy as np
 import cv2
+from edcamcam_testfile_shape import shapedetector
 import matplotlib.pyplot as plt
 
 
-matr=np.matrix(([[0,0, 1004], 0, 0, 0, 0,0, 0],
-               [0, 0, [101,  28, 130],[202,  28, 127],[303,  28, 124],[404,  29, 120],[505,  29, 117]]),
-               dtype=np.ndarray)
+#matr_generator
 
+p=1
+q=1
 
-print matr.shape
-print matr
+while p<4:
+    while q<4:
+        matr = np.zeros((p, q), dtype=np.uint8)
+        print matr
+        k=0
+        while k < 2**(p+q-1)-1:
+            g=0
+            while g < p*q:
+                i=0
+                j=0
+                while i < p:
 
-matr_shape = matr.shape
+                    while j < q:
+                        if matr[i, j] == 1:
+                            matr[i, j] == 0
+                        elif matr[i, j] == 0:
+                            matr[i, j] = 1
+                            print matr
+                            break
+                        j = j + 1
+                    else:
+                        continue
 
-spa_X_array = np.array([])
-spa_Y_array = np.array([])
-val_X_array = np.zeros((matr_shape[0], matr_shape[1]), dtype=np.uint32)
-val_Y_array = np.zeros((matr_shape[0], matr_shape[1]), dtype=np.uint32)
-n_row_matr = matr_shape[0]
-n_col_matr = matr_shape[1]
-matr_row_count = 0
-while matr_row_count < n_row_matr:
-    matr_col_count = 0
-    while matr_col_count < n_col_matr:
-        matr_value = matr[matr_row_count, matr_col_count]
-        if matr_value!=0:
-            val_X_array[matr_row_count, matr_col_count] = matr_value[0]
-            val_Y_array[matr_row_count, matr_col_count] = matr_value[1]
-        else:
-            val_X_array[matr_row_count, matr_col_count] = 0
-            val_Y_array[matr_row_count, matr_col_count] = 0
-        matr_col_count = matr_col_count + 1
-    matr_row_count = matr_row_count + 1
+                    break
+                    i = i + 1
+                g=g+1
+            k=k+1
+        q=q+1
+    p=p+1
 
-print val_X_array
-print val_Y_array
